c338ba2d8db796dce672ae4e9a93ed44d827e965
gosho-kazuya <gosho-kazuya@919.jp>
2016-04-04 21:34:34 +0900

    Fix dir bug

--- tree-json.py
+++ tree-json.py
@@ -4,21 +4,21 @@ import os
 import json
 
 def json_tree(path):
-	target_json = []
-	for item in os.listdir(path):
-		full_path = os.path.join(path,item)
-		new_hash = {}
-		new_hash['text'] = item
-		if os.path.isdir(full_path):
-			new_hash['nodes'] = []
-			child_dir = json_tree(full_path)
-			new_hash['nodes'].append(child_dir)
-		else:
-			new_hash['href'] = full_path.replace(os.getcwd(),'')
-		target_json.append(new_hash)
-	return target_json
+    target_json = []
+    for item in os.listdir(path):
+        full_path = os.path.join(path,item)
+        new_hash = {}
+        new_hash['text'] = item
+        if os.path.isdir(full_path):
+            #new_hash['nodes'] = []
+            child_dir = json_tree(full_path)
+            new_hash['nodes'] = child_dir
+        else:
+            new_hash['href'] = full_path.replace(os.getcwd(),'')
+        target_json.append(new_hash)
+    return target_json
 
 if __name__ == '__main__':
-	path = os.getcwd()+'/files'
-	print(json.dumps(json_tree(path)))
+    path = os.getcwd()+'/files'
+    print(json.dumps(json_tree(path)))
 
